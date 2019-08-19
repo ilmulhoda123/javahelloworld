6c38903bf593a001c3e76d6a7a80e2665374ad38
ilmulhoda123 <52668131+ilmulhoda123@users.noreply.github.com>
2019-08-19 17:48:32 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "-----this command will compile and run java helloworld programme for jenkins pipeline-----"
-javac javahelloworld/helloworld.java
+sudo javac javahelloworld/helloworld.java
 java -cp javahelloworld helloworld
